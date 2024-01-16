8c3c609ab085c28f04cf5069f9fc21d5371b4088
Meet Patel <meetgodad91048@gmail.com>
2024-01-15 19:11:07 -0700

    Rootpage edited

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,10 +1,12 @@
-import Link from "next/link";
+import Link from 'next/link';
 
-function StudentInfo() {
-    return (
-      <div>
-        <h1>Meet Hasmukhbhai Patel</h1>
-        <Link href="https://github.com/MeetGodad/cprg306-assignments">GitHub Repository</Link>
-      </div>
-    );
-  };
+const StudentInfo = () => {
+  return (
+    <div>
+      <p>Meet Patel</p>
+      <Link href="https://github.com/MeetGodad/cprg306-assignments">GitHub Repository</Link>
+    </div>
+  );
+};
+
+export default StudentInfo;
