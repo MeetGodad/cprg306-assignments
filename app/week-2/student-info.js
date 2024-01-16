78a6085365fbf1d5f920757ba1d74a1604b55e12
Meet Patel <meetgodad91048@gmail.com>
2024-01-15 18:19:31 -0700

    Component Created

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+function StudentInfo() {
+    return (
+      <div>
+        <h1>Meet Hasmukhbhai Patel</h1>
+        <Link href="https://github.com/MeetGodad/cprg306-assignments">GitHub Repository</Link>
+      </div>
+    );
+  };
