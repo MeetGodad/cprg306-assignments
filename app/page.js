0df5d719cc2b20b3bebf295f3663846dfd7b527d
Meet Patel <meetgodad91048@gmail.com>
2024-01-21 13:53:33 -0700

    CSS Added

--- app/page.js
+++ app/page.js
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Page() {
   return(
-    <main>
-      <h1>CPRG-306: Web Development 2 - Assignments</h1>
-      <Link href="week-2">Week 2</Link>
-    </main>
+    <main className="container mx-auto p-4 bg-gray-200 min-h-screen">
+    <h1 className="text-4xl font-extrabold text-blue-500 mb-8">CPRG-306: Web Development 2 - Assignments</h1>
+    <Link href="week-2" className="text-blue-500 hover:text-green-500 transition duration-500">Week 2 Assignments</Link>
+  </main>
   );
 };
