a2f7a2cb4f00754b39d2bcc11625f079185837d7
Meet Patel <meetgodad91048@gmail.com>
2024-03-18 13:50:50 -0600

    week-8-done

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -1,20 +1,26 @@
 "use client";
 
 import { useUserAuth } from "./_utils/auth-context";
+import Link from "next/link";
 
 export default function Page() {
   const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
 
-  console.log(user);
+  
+
 
   return (
     <div>
-      {<h1>Welcome, {user?.displayName}</h1>}
-      <p>Your email is {user?.email}</p>
-      {user ? (
-        <button onClick={firebaseSignOut}>Sign Out</button>
+      {user  
+      ? (
+        <>
+          <h1>Welcome, {user?.displayName}</h1>
+          <p>Your email is {user?.email}</p>
+          <button onClick={firebaseSignOut}>Sign Out</button>
+          <Link href="./week-8/shoping-list" className="flex">Go to Shopping List</Link>
+        </>
       ) : (
-        <button onClick={gitHubSignIn}>Sign In with GitHub</button>
+        <button className="text-xl m-8" onClick={gitHubSignIn}>Sign In with GitHub</button>
       )}
     </div>
   );
---
+++ app/week-8/shoping-list/page.js
@@ -0,0 +1,50 @@
+"use client"
+
+import  { useState } from "react";
+import ItemList from "./item-list";
+import NewItem from "./new-item";
+import itemsData from "./items.json";
+import MealIdeas from "./meal-ideas";
+import { useUserAuth } from "../_utils/auth-context";
+import Link from "next/link";
+
+
+
+export default function Page () {
+   
+  const[items, setItems] = useState(itemsData);
+  const [selectedItemName, setSelectedItemName] = useState("");
+  const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+
+  function handleItemSelect(item) {
+    const cleanedItemName = item.name
+      .split(',')[0] // Remove the size
+      .replace(/[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2580-\u27BF]|\uD83E[\uDD10-\uDDFF]/g, '') // Remove the emoji
+      .trim();
+    setSelectedItemName(cleanedItemName);
+  }
+  
+  function handleAddItem(newItem) {
+    setItems([...items, newItem]);
+  }
+
+  return (
+    user ? (
+      <main>
+        <div className="flex">
+        <div className="max-w-sm m-2">
+          <h1 className="text-4xl font-semibold mb-4">Shopping List</h1>
+            <div className=" pr-2">
+              <NewItem onAddItem={handleAddItem} />
+              <ItemList items={items} onItemSelect={handleItemSelect} />
+            </div>
+          </div>
+          <div className="w-1/2 pl-2">
+              <MealIdeas ingredient={selectedItemName} />
+            </div>
+        </div>
+      </main>
+    ) : 
+   <Link href="./" className="flex text-lg m-8">Go Back an Sign In </Link>
+  );
+};
